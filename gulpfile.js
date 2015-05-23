0f0a56feab6d28de531d6bdab0d82f07a3357142
noyobo <nongyoubao@alibaba-inc.com>
2015-05-23 12:12:25 +0800

    生产 MD 文件对应 JS 文件

--- gulpfile.js
+++ gulpfile.js
@@ -6,6 +6,7 @@ var rename = require('gulp-rename');
 var copy = require('gulp-copy');
 var watch = require('gulp-watch');
 var map = require('map-stream');
+var through2 = require('through2');
 var babel = require('babel-core');
 var markdown = require('./lib/markdown.js');
 var render = require('./lib/xtpl.js');
@@ -14,27 +15,32 @@ var path = require('path');
 var fs = require('fs');
 
 var mdList = [];
-var mdToHtml = map(function(file, done) {
-  var mdData = markdown(String(file.contents), file.path);
-  var html = render(mdData, 'example');
-  var relativePath = path.relative(process.cwd(), file.path);
 
+var mdToHtml = through2.obj(function(file, enc, done) {
+  var self = this;
+  var mdData = markdown(String(file.contents), file.path);
+  var html = render(mdData.data, 'example');
+
+  file.path = file.path.replace(/\.md$/, '.html')
+  var relativePath = path.relative(process.cwd(), file.path);
   mdList.push({
-    url: relativePath.replace(/\.md$/, '.html'),
-    title: mdData.title
+    url: relativePath,
+    title: mdData.data.title
   })
   file.contents = new Buffer(html);
   console.log('create html file for:', file.path);
-  return done(null, file)
+  self.push(file)
+  while (mdData.jsFiles.length > 0) {
+    var jsfile = mdData.jsFiles.pop();
+    self.push(jsfile)
+  }
+  done()
 })
 
 gulp.task('md', function() {
   return gulp
     .src(['./**/*.md', '!./node_modules/**/*.md', '!README.md'])
     .pipe(mdToHtml)
-    .pipe(rename({
-      extname: '.html'
-    }))
     .pipe(gulp.dest('./_site'))
     .on('end', function() {
       var listHtml = render({
